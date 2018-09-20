2c2651107c4af0462b1e215b0d86b012848c2a72
McKenna Cash <mckenna.cash.2020@student.lhschools.org>
2018-09-20 08:25:03 -0400

    Initializing code

---
+++ src/numberGuessing.java
@@ -0,0 +1,21 @@
+import static java.lang.Math.random;
+
+public class numberGuessing {
+
+
+    public static void main(String[]args){
+
+    int pickedNumber;
+
+
+    pickedNumber =(int) (Math.random()*100);
+
+    System.out.println(pickedNumber);
+
+
+
+
+
+
+    }
+}
