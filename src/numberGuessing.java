e38f3b24acdc2b687989adc12dae51d4e670401f
McKenna Cash <mckenna.cash.2020@student.lhschools.org>
2018-09-21 08:15:41 -0400

    Done

--- src/numberGuessing.java
+++ src/numberGuessing.java
@@ -3,61 +3,25 @@ import java.util.Scanner;
 import static java.lang.Math.random;
 
 public class numberGuessing {
-
-
     public static void main(String[]args){
 
 
 Scanner keyboard;
     int pickedNumber, userInput;
+
     keyboard = new Scanner(System.in);
-
-
     pickedNumber =(int) (Math.random()*100);
-        System.out.println(pickedNumber);
-    System.out.println("Pick a number between 0-100");
-        userInput = keyboard.nextInt();
+        do {
 
-        for (int pickedNumber: userInput){
-
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       /* if (userInput==pickedNumber){
-            System.out.println("You win!");
-
-        }
-        else if(userInput<pickedNumber){
-
-            System.out.println("Too low! Guess higher");
+            System.out.println("Pick a number between 0-100");
             userInput = keyboard.nextInt();
-        }
-        else if (userInput>pickedNumber){
-
-            System.out.println("Too high! Guess lower");
-            userInput = keyboard.nextInt();
-        }*/
-
-
-
-
-
-
-
-
+            if (userInput==pickedNumber){
+                System.out.println("You got it!");
+            }
+            else if (userInput>pickedNumber)
+                System.out.println("Too high");
+            else if (userInput<pickedNumber)
+            System.out.println("Too low");
+        }while(pickedNumber!=userInput);
     }
 }
