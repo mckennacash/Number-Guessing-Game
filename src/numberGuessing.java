4bd024eef87d69a30ae71dde0f7205d98f11e7f2
McKenna Cash <mckenna.cash.2020@student.lhschools.org>
2018-09-27 08:49:26 -0400

    updating code

--- src/numberGuessing.java
+++ src/numberGuessing.java
@@ -3,25 +3,44 @@ import java.util.Scanner;
 import static java.lang.Math.random;
 
 public class numberGuessing {
-    public static void main(String[]args){
+    public static void main(String[]args) {
 
 
-Scanner keyboard;
-    int pickedNumber, userInput;
+        Scanner keyboard;
+        int pickedNumber, userInput;
 
-    keyboard = new Scanner(System.in);
-    pickedNumber =(int) (Math.random()*100);
+        String userAnswer;
+        String userContinue;
+        boolean game = false;
+
+        keyboard = new Scanner(System.in);
         do {
+            pickedNumber = (int) (Math.random() * 100);
 
-            System.out.println("Pick a number between 0-100");
-            userInput = keyboard.nextInt();
-            if (userInput==pickedNumber){
-                System.out.println("You got it!");
-            }
-            else if (userInput>pickedNumber)
-                System.out.println("Too high");
-            else if (userInput<pickedNumber)
-            System.out.println("Too low");
-        }while(pickedNumber!=userInput);
+              do {
+                  System.out.println("Pick a number between 0-100");
+                  userInput = keyboard.nextInt();
+                  keyboard.nextLine();
+                  if (userInput == pickedNumber) {
+                      System.out.println("You got it!");
+                  } else if (userInput > pickedNumber)
+                      System.out.println("Too high");
+                  else if (userInput < pickedNumber)
+                      System.out.println("Too low");
+              } while (pickedNumber != userInput);
+
+
+
+
+           System.out.println("Want to play again?");
+
+            userAnswer = keyboard.nextLine();
+            if (userAnswer.equalsIgnoreCase("no")){break;}
+
+        }while (!userAnswer.equalsIgnoreCase("no"));
+
+
+
+
     }
 }
