c4ff373921b0f91c9918edb80722c7e1e19f5b0f
McKenna Cash <mckenna.cash.2020@student.lhschools.org>
2018-09-28 08:15:40 -0400

    updating

--- src/numberGuessing.java
+++ src/numberGuessing.java
@@ -8,7 +8,7 @@ public class numberGuessing {
 
         Scanner keyboard;
         int pickedNumber, userInput;
-
+        int tries = 0;
         String userAnswer;
         String userContinue;
         boolean game = false;
@@ -16,7 +16,8 @@ public class numberGuessing {
         keyboard = new Scanner(System.in);
         do {
             pickedNumber = (int) (Math.random() * 100);
-
+            System.out.println("You have 6 tries to guess the right number. Want to play?");
+            keyboard.nextLine();
               do {
                   System.out.println("Pick a number between 0-100");
                   userInput = keyboard.nextInt();
@@ -25,19 +26,18 @@ public class numberGuessing {
                       System.out.println("You got it!");
                   } else if (userInput > pickedNumber)
                       System.out.println("Too high");
-                  else if (userInput < pickedNumber)
+                  if (userInput < pickedNumber)
                       System.out.println("Too low");
-              } while (pickedNumber != userInput);
 
-
-
+              } while (tries !=6 && pickedNumber != userInput);
 
            System.out.println("Want to play again?");
+
 
             userAnswer = keyboard.nextLine();
             if (userAnswer.equalsIgnoreCase("no")){break;}
 
-        }while (!userAnswer.equalsIgnoreCase("no"));
+        }while(userAnswer.equalsIgnoreCase("no"));
 
 
 
